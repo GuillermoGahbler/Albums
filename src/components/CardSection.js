d64dc66018fe834430ff65588c0c7f4402d10a80
Guillermo Gahbler <mexamericano@gmail.com>
2018-10-17 11:15:12 -0700

    added styling to cardSection component

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -7,8 +7,20 @@ const CardSection = () =>{
     <View>
 
     </View>
-  )
+  );
 
-}
+};
+
+const style = {
+  containerStyle:{
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  }
+};
 
 export default CardSection;

0c53cbe26b728aae207572f39dea29ed09e2d3b3
Guillermo Gahbler <mexamericano@gmail.com>
2018-10-22 14:56:38 -0700

    created Button component and added button to AlbumtDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 
 const AlbumDetail = ({ album }) => {
@@ -27,11 +28,17 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+      
       <CardSection>
-        <Image 
-        style={imageStyle}
-        source={{uri: image }}/>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }} />
       </CardSection>
+      
+      <CardSection>
+        <Button/>
+      </CardSection>
+
     </Card>
   );
 };
@@ -54,9 +61,9 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
-  }, 
-  imageStyle:{
-    height:300,
+  },
+  imageStyle: {
+    height: 300,
     flex: 1,
     width: null
   }
